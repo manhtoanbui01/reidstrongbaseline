799303e59c7ef96a2e04c815d76911e903e586e4
manhtoanbui <manhtoan1.bui@gmail.com>
2025-06-06 15:16:42 +0900

    nightreid_tiktok_dataloader

--- data/datasets/__init__.py
+++ data/datasets/__init__.py
@@ -13,6 +13,7 @@ from .dataset_loader import ImageDataset
 from .nightreid import NightReID
 from .nightreid_plus_market1501 import NightReID_Market1501
 from .night600_plus_market1501 import Night600_Market1501
+from .nightreid_plus_tiktok import NightReID_TikTok
 
 __factory = {
     'market1501': Market1501,
@@ -23,7 +24,8 @@ __factory = {
     'night600': Night600,
     'nightreid': NightReID,
     'nightreid_market1501': NightReID_Market1501,
-    'night600_market1501': Night600_Market1501
+    'night600_market1501': Night600_Market1501,
+    'nightreid_tiktok': NightReID_TikTok
 }
 
 
